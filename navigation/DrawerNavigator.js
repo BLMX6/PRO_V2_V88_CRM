840444dc59df9e0d5116cccc8ca8774546fa21b1
BLMX6 <119624247+BLMX6@users.noreply.github.com>
2022-12-05 13:28:58 -0600

    Update DrawerNavigator.js

--- navigation/DrawerNavigator.js
+++ navigation/DrawerNavigator.js
@@ -10,17 +10,17 @@ const DrawerNavigator = () => {
   return (
     <Drawer.Navigator screenOptions={{ headerShown: false }}>
       <Drawer.Screen
-        name="MyHome"
+        name="Inicio"
         component={StackNavigator}
         options={{ unmountOnBlur: true }}
       />
       <Drawer.Screen
-        name="Profile"
+        name="Perfil"
         component={Profile}
         options={{ unmountOnBlur: true }}
       />
       <Drawer.Screen
-        name="Logout"
+        name="CierreSesion"
         component={Logout}
         options={{ unmountOnBlur: true }}
       />
